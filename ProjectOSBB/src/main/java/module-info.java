a77ec4255bfdc7206c5795802b90d388df74b042
HelpImStuck <93922684+HelpImStuck@users.noreply.github.com>
2021-11-08 15:09:39 +0200

    Add files via upload

---
+++ ProjectOSBB/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.projectosbb {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires org.jetbrains.annotations;
+
+    opens com.example.projectosbb to javafx.fxml;
+    exports com.example.projectosbb;
+}

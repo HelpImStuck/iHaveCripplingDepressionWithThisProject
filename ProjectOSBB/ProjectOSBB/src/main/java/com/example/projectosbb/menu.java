e3d341e80566769de8878f4afd6c10cdb62c8b92
HelpImStuck <93922684+HelpImStuck@users.noreply.github.com>
2021-11-08 15:57:38 +0200

    Add files via upload

---
+++ ProjectOSBB/ProjectOSBB/src/main/java/com/example/projectosbb/menu.java
@@ -0,0 +1,99 @@
+package com.example.projectosbb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+public class menu {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ScrollBar MenuScroll1;
+
+    @FXML
+    private HBox MenuScroll2;
+
+    @FXML
+    private DatePicker MenuDate1;
+
+    @FXML
+    private Label TextFieldMenu1;
+
+    @FXML
+    private AnchorPane AnchorPaneMenu2;
+
+    @FXML
+    private Label TextFieldMenu3;
+
+    @FXML
+    private Label TextFieldMenu2;
+
+    @FXML
+    private Button MenuBoxButton3;
+
+    @FXML
+    private Button MenuBoxButton11;
+
+    @FXML
+    private AnchorPane AnchorPaneMenu;
+
+    @FXML
+    private RadioButton MenuRadio1;
+
+    @FXML
+    private Button MenuBoxButton2;
+
+    @FXML
+    private Button MenuBoxButton12;
+
+    @FXML
+    private RadioButton MenuRadio2;
+
+    @FXML
+    private Button MenuBoxButton5;
+
+    @FXML
+    private RadioButton MenuRadio3;
+
+    @FXML
+    private Button MenuBoxButton4;
+
+    @FXML
+    private Button MenuBoxButton10;
+
+    @FXML
+    private Button MenuBoxButton1;
+
+    @FXML
+    private ToggleButton MenuToggle1;
+
+    @FXML
+    private Button MenuBoxButton7;
+
+    @FXML
+    private Button MenuBoxButton6;
+
+    @FXML
+    private Button MenuBoxButton9;
+
+    @FXML
+    private Button MenuBoxButton8;
+
+    @FXML
+    void initialize() {
+
+    }
+}
---
+++ ProjectOSBB/ProjectOSBB/src/main/java/com/example/projectosbb/registration.java
@@ -0,0 +1,59 @@
+package com.example.projectosbb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class registration {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button RegExit1;
+
+    @FXML
+    private AnchorPane AnchorPaneReg2;
+
+    @FXML
+    private CheckBox Check1;
+
+    @FXML
+    private AnchorPane AnchorPaneReg1;
+
+    @FXML
+    private TextField RegMail1;
+
+    @FXML
+    private CheckBox Check2;
+
+    @FXML
+    private Label RegistrationMenuText2;
+
+    @FXML
+    private TextField RegLog1;
+
+    @FXML
+    private Label RegistrationMenuText1;
+
+    @FXML
+    private PasswordField RegPas1;
+
+    @FXML
+    private Button RegEnter1;
+
+    @FXML
+    private PasswordField RegPas2;
+
+    }
+
+
